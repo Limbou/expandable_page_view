1bd4ca65beb58aaf7f6f8069cfaae0805d38ab42
Jakub Danielczyk <jakubdanielczyk@MacBook-Pro-Jakub.local>
2021-02-11 16:15:35 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# expandable_page_view
+
+A PageView widget adjusting it's height to currently displayed page
+
+## Getting Started
---
+++ lib/expandable_page_view.dart
@@ -0,0 +1,7 @@
+library expandable_page_view;
+
+/// A Calculator.
+class Calculator {
+  /// Returns [value] plus 1.
+  int addOne(int value) => value + 1;
+}
