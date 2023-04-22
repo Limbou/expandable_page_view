4c4fb43368de9856dae5d2518fff7cc017e8d141
Limbou <29557232+Limbou@users.noreply.github.com>
2023-04-22 09:42:19 +0200

    chore: upgrade gradle and xcode configs

--- example/lib/page/balance_page.dart
+++ example/lib/page/balance_page.dart
@@ -1,6 +1,10 @@
-import 'package:example/page/page.dart';
+import 'package:example/page/view/available_balance_tile.dart';
+import 'package:example/page/view/cards_tile.dart';
+import 'package:example/page/view/contacts_tile.dart';
 import 'package:expandable_page_view/expandable_page_view.dart';
 import 'package:flutter/material.dart';
+
+import 'view/widgets/hide_cards_button.dart';
 
 class BalancePage extends StatelessWidget {
   const BalancePage({Key? key}) : super(key: key);
